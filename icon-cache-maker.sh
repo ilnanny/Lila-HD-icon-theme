ac88a7292c422da85efa47117c3cde1ce09b600f
ilnanny <ilnannyhack@gmail.com>
2017-07-01 21:37:02 +0200

    update

---
+++ icon-cache-maker.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Lila-HD-icon-theme
+#Icon theme for Gnu Linux Operation Sistem 
+#---------------------------------------------#
+
+
+
+echo "making of icon cache for Lila-HD-icon-theme
+
+gtk-update-icon-cache -f /usr/share/icons/Lila_HD
+gtk-update-icon-cache -f /usr/share/icons/Lila_HD_Kaki
+gtk-update-icon-cache -f /usr/share/icons/Lila_HD_Blue
+gtk-update-icon-cache -f /usr/share/icons/Lila_HD_Dark
+gtk-update-icon-cache -f /usr/share/icons/Lila_HD_Green
+gtk-update-icon-cache -f /usr/share/icons/Lila_HD_Light-Grey
+gtk-update-icon-cache -f /usr/share/icons/Lila_HD/default
+
